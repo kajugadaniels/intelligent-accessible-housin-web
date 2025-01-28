9a0910a5e23efddd2579f69cf96a1bb36b45ba44
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-28 19:38:59 +0200

    New changes

--- horizonConstruction/urls.py
+++ horizonConstruction/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
-    path('admin/', include('base.urls')),
+    path('admin/', include('backend.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
