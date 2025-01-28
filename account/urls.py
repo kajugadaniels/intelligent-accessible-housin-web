f302aa38ad3d08f83dfd1f5222510632aee99570
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-28 19:01:33 +0200

    New changes

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,7 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    path('', user_login, name="login"),
-    path('logout/', user_logout, name='logout'),
-
-    path('profile/', editProfile, name='editProfile'),
+    path('', userLogin, name="login"),
+    path('logout/', userLogout, name='logout'),
+    path('profile/', userProfile, name='userProfile'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
