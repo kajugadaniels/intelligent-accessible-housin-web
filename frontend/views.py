9ee1e161b99ca467b4bc917967b9033fb4adf001
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-06 16:21:52 +0200

    Make home function retrieve properties

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from backend.models import *
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return render(request, 'frontend/pages/index.html')
+    """
+    Home view: Display the homepage with the 4 latest properties.
+    """
+    properties = Property.objects.order_by('-created_at')[:4]
+
+    context = {
+        'properties': properties,
+    }
+
+    return render(request, 'frontend/pages/index.html', context)
 
 def getProperties(request):
     return render(request, 'frontend/pages/properties/index.html')
