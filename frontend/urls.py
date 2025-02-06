25d632fb651358e5d3780a1b2d4ae8f1febd32bd
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-06 13:02:24 +0200

    Setup home page

---
+++ frontend/urls.py
@@ -0,0 +1,10 @@
+from frontend.views import *
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('', home, name="home"),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
