46d0548173f83c5c1c1f26f4959386ae0efcfc4d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-06 14:19:58 +0200

    Setup properties page

--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'frontend/pages/index.html')
+
+def getProperties(request):
+    return render(request, 'frontend/pages/property.html')
