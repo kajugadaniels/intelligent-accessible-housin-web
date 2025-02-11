7605307069d276354aa4f05cc3e287210c511d6c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-11 13:07:05 +0200

    Neww changes

--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,7 @@ from frontend.forms import *
 from backend.models import *
 from django.urls import reverse
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
