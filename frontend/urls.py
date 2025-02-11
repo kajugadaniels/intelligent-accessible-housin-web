8a7f5ec5fcb98d72031a32e644dadc7ae2d71e67
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-11 17:21:24 +0200

    Define URL for RentApplication form

--- frontend/urls.py
+++ frontend/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
 
     path('properties', getProperties, name="getProperties"),
     path('property/<slug:slug>/', showProperty, name="showProperty"),
+    path('property/<slug:slug>/apply/', sendRentApplication, name='sendRentApplication'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
