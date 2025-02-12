7c62fd978ab3c5a6b9a874329d4e9fa38128535d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-12 13:06:45 +0200

    Define URLs for getting user applications and viewing application details

--- frontend/urls.py
+++ frontend/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     path('property/<slug:slug>/apply/', sendRentApplication, name='sendRentApplication'),
 
     path('user/dashboard', userDashboard, name="userDashboard"),
-    path('user/applications', getUserApplication, name="getUserApplication"),
+    path('user/applications/', getUserApplication, name='getUserApplications'),
+    path('user/application/<int:application_id>/', applicationDetail, name='applicationDetail'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
