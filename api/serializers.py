5d556eb56c880311d699615e33fb9a3b6df78df8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-18 11:11:49 +0200

    Add RegisterSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.password_validation import validate_password
 
@@ -37,3 +38,40 @@ class LoginSerializer(serializers.Serializer):
         attrs['access'] = str(refresh.access_token)
 
         return attrs
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering a new user.
+    """
+    password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['name', 'email', 'phone_number', 'password', 'confirm_password']
+    
+    def validate(self, attrs):
+        password = attrs.get('password')
+        confirm_password = attrs.get('confirm_password')
+
+        if password != confirm_password:
+            raise serializers.ValidationError("The two password fields didn’t match.")
+        
+        # Validate the password according to Django's password validation settings
+        try:
+            validate_password(password)
+        except ValidationError as error:
+            raise serializers.ValidationError({"password": error.messages})
+
+        return attrs
+
+    def create(self, validated_data):
+        # Remove confirm_password since it's not stored
+        validated_data.pop('confirm_password')
+        user = get_user_model().objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            name=validated_data['name'],
+            phone_number=validated_data['phone_number'],
+        )
+        return user
