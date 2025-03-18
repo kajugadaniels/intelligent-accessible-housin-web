4ce4c678b86072a8d03df3d108d1f13a000c814d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-18 11:22:49 +0200

    Add property and property details routes

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ app_name = 'api'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('properties/', GetPropertiesView.as_view(), name='getProperties'),
+    path('property/<int:id>/', ShowPropertyView.as_view(), name='showProperty'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
