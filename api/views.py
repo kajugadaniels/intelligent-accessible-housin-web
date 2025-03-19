19eed1bf7c5fb36c31d30d0f3b52f34edd241b1c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-20 01:33:39 +0200

    New changes

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from api.serializers import *
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.exceptions import InvalidToken
 
 class LoginView(APIView):
