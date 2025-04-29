91ed61d6d227ea6ae449efd99042300a7c72772c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-29 19:17:43 +0200

    New changes

--- isc/urls.py
+++ isc/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
-    path('admin/', include('backend.urls')),
-    path('', include('frontend.urls')),
+    path('', include('backend.urls')),
+    # path('old/', include('frontend.urls')),
     path('api/', include('api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
