c198e9abb14174ed81c86bb190cda1e7366e1173
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-29 19:47:12 +0200

    Add search route and view function

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('dashboard/', dashboard, name="dashboard"),
+    path('search/', search, name="search"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -9,3 +9,9 @@ def dashboard(request):
         raise PermissionDenied(_("You are not authorized to view the dashboard."))
 
     return render(request, 'backend/pages/users/dashboard.html')
+
+def search(request):
+    if request.user.role not in ['User'] and not request.user.is_superuser:
+        raise PermissionDenied(_("You are not authorized to view the dashboard."))
+
+    return render(request, 'backend/pages/users/search.html')
