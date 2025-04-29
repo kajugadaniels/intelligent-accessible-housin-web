b08af1e931642eb486d559fe0a9d61e3b03f62a7
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-29 19:46:08 +0200

    Add user dashboard link in sidebar

--- users/views.py
+++ users/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 def dashboard(request):
+    if request.user.role not in ['User'] and not request.user.is_superuser:
+        raise PermissionDenied(_("You are not authorized to view the dashboard."))
+
     return render(request, 'backend/pages/users/dashboard.html')
