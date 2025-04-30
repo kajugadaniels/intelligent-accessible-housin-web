8a04718447e4ae15a53567dd7bcf2e1a4ad408e9
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-30 16:45:10 +0200

    New changes

--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
 
     path('notifications/', notifications, name="notifications"),
 
-    path('sendApplication/<int:id>/apply/', sendApplication, name="sendApplication"),
+    path('property/<int:id>/apply/', sendApplication, name="sendApplication"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
