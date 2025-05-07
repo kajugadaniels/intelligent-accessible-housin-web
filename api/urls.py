96746b9ab3ae0382f0540a0a8d148d61b8d8a5a2
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-07 19:27:31 +0200

    Register AcceptContractAPIView URL route

--- api/urls.py
+++ api/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('property/<int:id>/apply/', SendApplicationAPIView.as_view(), name='sendApplication'),
 
     path('contracts/', ContractsAPIView.as_view(), name='getContracts'),
+    path('contracts/<int:contract_id>/accept/', AcceptContractAPIView.as_view(), name='acceptContract'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
