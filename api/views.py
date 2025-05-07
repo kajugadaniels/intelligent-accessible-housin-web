4d2ba8166890ce1f44d707a181b47d83d751878d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-07 19:20:20 +0200

    Add RentApplicationSerializer

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from api.serializers import *
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError
