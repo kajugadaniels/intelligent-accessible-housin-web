b52d0820f6ea2cc5c24ec878f04c06dd4d5a470f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-07 18:21:00 +0200

    New changes

--- isc/settings.py
+++ isc/settings.py
@@ -83,24 +83,28 @@ WSGI_APPLICATION = 'isc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if str(os.getenv("NODE_ENV"))=="production":
+ENV = os.getenv("NODE_ENV", "development")
+
+if ENV == "production":
+    # use sqlite in your production environment
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
-    DATABASES =  {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB", ""),
-        'USER': os.getenv("MYSQL_USER", ""),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD", ""),
-        'HOST': os.getenv("MYSQL_HOST", ""),
-        'PORT': os.getenv("MYSQL_PORT", ""),
+    # use MySQL locally
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("MYSQL_DB", ""),
+            "USER": os.getenv("MYSQL_USER", ""),
+            "PASSWORD": os.getenv("MYSQL_PASSWORD", ""),
+            "HOST": os.getenv("MYSQL_HOST", ""),
+            "PORT": os.getenv("MYSQL_PORT", ""),
+        }
     }
-}
 
 # DATABASES =  {
 #     'default': {
